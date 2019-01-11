9dcc5308955cb9ebadcf1b07f90ca07752ba7cd2
Julien Delvaux <delvaux.ju@gmail.com>
2019-01-11 11:51:03 +0100

    test(circular-byte-buffer):add 2 tests to test circularity of buffer test(circular-byte-buffer):add 2 tests verify return in case of buffer full or empty

--- test/test_circular-byte-buffer.c
+++ test/test_circular-byte-buffer.c
@@ -38,7 +38,7 @@ void test_cbb_read_byte()
     circ_bbuf_push(&buffer, 0x10);
 
     TEST_ASSERT_EQUAL_UINT8(CBB_SUCCESS, circ_bbuf_pop(&buffer, &poped_from_buffer));
-    TEST_ASSERT_EQUAL_UINT8(poped_from_buffer, 0x10);
+    TEST_ASSERT_EQUAL_UINT8(0x10, poped_from_buffer);
 
     TEST_ASSERT_EQUAL_UINT32(buffer_size, circ_bbuf_available_space(&buffer));
     TEST_ASSERT_EQUAL_UINT32(0, circ_bbuf_available_bytes_to_read(&buffer));
@@ -46,7 +46,22 @@ void test_cbb_read_byte()
     TEST_ASSERT_EQUAL_UINT8(FALSE, circ_bbuf_is_full(&buffer));
 }
 
-void test_cbb_insert_bytes_to_full_array()
+void test_cbb_push_byte_buffer_full_test()
+{
+    uint8_t input_array[]= {11,22,33,44,55,66,77,88};
+
+    circ_bbuf_push_bytes(&buffer, input_array, buffer_size);
+    TEST_ASSERT_EQUAL_UINT8(CBB_BUFFER_FULL, circ_bbuf_push(&buffer, 0x10));
+}
+
+void test_cbb_pop_byte_buffer_empty_test()
+{
+    uint8_t poped_from_buffer = 0x98;
+    TEST_ASSERT_EQUAL_UINT8(CBB_BUFFER_EMPTY, circ_bbuf_pop(&buffer, &poped_from_buffer));
+    TEST_ASSERT_EQUAL_UINT8(0x98, poped_from_buffer);
+}
+
+void test_cbb_push_bytes_to_full_array()
 {
     uint8_t input_array[]= {10,20,30,40,50,60,70,80};
 
@@ -65,6 +80,43 @@ void test_cbb_pop_bytes_to_empty_array()
 
     TEST_ASSERT_EQUAL_UINT8(CBB_SUCCESS, circ_bbuf_pop_bytes(&buffer, buffer_size, popped_array));
     TEST_ASSERT_EQUAL_UINT8_ARRAY(input_array, popped_array, buffer_size);
+
+    TEST_ASSERT_EQUAL_UINT32(buffer_size, circ_bbuf_available_space(&buffer));
+    TEST_ASSERT_EQUAL_UINT32(0, circ_bbuf_available_bytes_to_read(&buffer));
+    TEST_ASSERT_EQUAL_UINT8(TRUE, circ_bbuf_is_empty(&buffer));
+    TEST_ASSERT_EQUAL_UINT8(FALSE, circ_bbuf_is_full(&buffer));
+}
+
+void test_cbb_push_bytes_circular_test()
+{
+    uint8_t input_array[]= {11,22,33,44,55,66,77,88};
+    uint8_t input_array2[]= {15,26,37};
+    uint8_t popped_array[8];
+
+    circ_bbuf_push_bytes(&buffer, input_array, buffer_size);
+    circ_bbuf_pop_bytes(&buffer, buffer_size, popped_array);
+
+    TEST_ASSERT_EQUAL_UINT8(CBB_SUCCESS, circ_bbuf_push_bytes(&buffer, input_array, sizeof(input_array2)));
+    TEST_ASSERT_EQUAL_UINT32((buffer_size - sizeof(input_array2)), circ_bbuf_available_space(&buffer));
+    TEST_ASSERT_EQUAL_UINT32(sizeof(input_array2), circ_bbuf_available_bytes_to_read(&buffer));
+    TEST_ASSERT_EQUAL_UINT8(FALSE, circ_bbuf_is_empty(&buffer));
+    TEST_ASSERT_EQUAL_UINT8(FALSE, circ_bbuf_is_full(&buffer));
+}
+
+void test_cbb_pop_bytes_circular_to_empty_array()
+{
+    uint8_t input_array[]= {11,22,33,44,55,66,77,88};
+    uint8_t input_array2[]= {15,26,37};
+    uint8_t popped_array[8];
+    uint8_t popped_array2[3];
+
+    circ_bbuf_push_bytes(&buffer, input_array, buffer_size);
+    circ_bbuf_pop_bytes(&buffer, buffer_size, popped_array);
+    circ_bbuf_push_bytes(&buffer, input_array2, sizeof(input_array2));
+
+
+    TEST_ASSERT_EQUAL_UINT8(CBB_SUCCESS, circ_bbuf_pop_bytes(&buffer, sizeof(input_array2), popped_array2));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(input_array2, popped_array2, sizeof(input_array2));
 
     TEST_ASSERT_EQUAL_UINT32(buffer_size, circ_bbuf_available_space(&buffer));
     TEST_ASSERT_EQUAL_UINT32(0, circ_bbuf_available_bytes_to_read(&buffer));
